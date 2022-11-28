463c4ffaacecb82a8c123d9b729cee29202ef795
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2022-11-28 13:21:05 -0300

    Form cep
    Atualizando

--- Form/src/app/template-form/template-form.component.ts
+++ Form/src/app/template-form/template-form.component.ts
@@ -1,36 +1,89 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
-  styleUrls: ['./template-form.component.scss']
+  styleUrls: ['./template-form.component.scss'],
 })
 export class TemplateFormComponent implements OnInit {
+  usuario: any = {
+    nome: null,
+    email: null,
+  };
 
-  usuario: any ={
-    nome:null,
-    email:null
-  }
-
-  onSubmit(form: any){
+  onSubmit(form: any) {
     console.log(form);
-   /*  console.log(this.usuario); */
+    console.log(this.usuario);
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  verificaValidTouched(campo: { valid: any; touched: any; }) {
+  verificaValidTouched(campo: { valid: any; touched: any }) {
     return !campo.valid && campo.touched;
   }
 
- aplicaCssErro(campo: { valid: any; touched: any; }){
-    return{
-     'has-error':this.verificaValidTouched(campo),
-     'has-feedback':this.verificaValidTouched(campo)
-    }
+  aplicaCssErro(campo: { valid: any; touched: any }) {
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo),
+    };
   }
 
+  consultaCEP(cep: any) {
+    cep = cep.replace(/\D/g,'');
+    if (cep!==''){
+      this.http
+      .get(`//viacep.com.br/ws/${cep}/json/`)
+      .subscribe(dados=>console.log(dados));
+    }
+  }
 }
+
+
+
+/* // ---- // ---- // ---- // ---- // ---- // ---- // ---- // ---- //
+
+Arquivo template-form.component.html:
+
+- dentro da Tag Input:
+
+name="cep" ngModel required #cep="ngModel"
+(blur)="consultaCEP(cep.value)" //utilizando a própria variável local
+
+// ---- // ---- // ---- // ---- // ---- // ---- // ---- // ---- //
+
+Arquivo template-form.module.ts:
+
+import { HttpClientModule } from '@angular/common/http';
+
+imports: [
+        HttpClientModule
+
+// ---- // ---- // ---- // ---- // ---- // ---- // ---- // ---- //
+
+Arquivo template-form.component.ts:
+
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+constructor(private http: HttpClient) { }
+
+consultaCEP(cep: any) {
+
+    cep = cep.replace (/\D/g, '');
+
+    if (cep != null && cep !== '') {
+      let validacep = /^[0-9]{8}$/;
+
+      if (validacep.test(cep)){
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json/`)
+          .pipe(map((dados: any) => dados))
+          .subscribe(dados => console.log(dados));
+      }
+    }
+}
+ */
--- Form/src/app/template-form/template-form.module.ts
+++ Form/src/app/template-form/template-form.module.ts
@@ -1,19 +1,23 @@
+import { TemplateFormComponent } from './template-form.component';
+import { HttpClientModule } from '@angular/common/http';
 import { CampoControlErroComponent } from './../campo-control-erro/campo-control-erro.component';
 import { FormDebugComponent } from './../form-debug/form-debug.component';
-import { FormsModule } from '@angular/forms';
-import { TemplateFormComponent } from './template-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 @NgModule({
   declarations: [
-    TemplateFormComponent,
     FormDebugComponent,
-    CampoControlErroComponent
+    CampoControlErroComponent,
+    TemplateFormComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
-  ]
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
 })
 export class TemplateFormModule { }
