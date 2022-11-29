9191812876c24dda47e8494f100b169146f72ca5
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2022-11-28 21:39:51 -0300

    Codigo limpo
    Cep

--- Form/src/app/template-form/template-form.component.ts
+++ Form/src/app/template-form/template-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template-form',
@@ -33,57 +34,24 @@ export class TemplateFormComponent implements OnInit {
   }
 
   consultaCEP(cep: any) {
+    cep = cep.replace(/\D/g, '');
+    if (cep != null && cep !== '') {
+      let validacep = /^[0-9]{8}$/;
+
+      if (validacep.test(cep)) {
+        this.http
+          .get(`//viacep.com.br/ws/${cep}/json/`)
+          .pipe(map((dados: any) => dados))
+          .subscribe((dados) => console.log(dados));
+      }
+    }
+  }
+  /*  consultaCEP(cep: any) {
     cep = cep.replace(/\D/g,'');
     if (cep!==''){
       this.http
       .get(`//viacep.com.br/ws/${cep}/json/`)
       .subscribe(dados=>console.log(dados));
     }
-  }
+  } */
 }
-
-
-
-/* // ---- // ---- // ---- // ---- // ---- // ---- // ---- // ---- //
-
-Arquivo template-form.component.html:
-
-- dentro da Tag Input:
-
-name="cep" ngModel required #cep="ngModel"
-(blur)="consultaCEP(cep.value)" //utilizando a própria variável local
-
-// ---- // ---- // ---- // ---- // ---- // ---- // ---- // ---- //
-
-Arquivo template-form.module.ts:
-
-import { HttpClientModule } from '@angular/common/http';
-
-imports: [
-        HttpClientModule
-
-// ---- // ---- // ---- // ---- // ---- // ---- // ---- // ---- //
-
-Arquivo template-form.component.ts:
-
-import { map } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
-
-constructor(private http: HttpClient) { }
-
-consultaCEP(cep: any) {
-
-    cep = cep.replace (/\D/g, '');
-
-    if (cep != null && cep !== '') {
-      let validacep = /^[0-9]{8}$/;
-
-      if (validacep.test(cep)){
-
-        this.http.get(`//viacep.com.br/ws/${cep}/json/`)
-          .pipe(map((dados: any) => dados))
-          .subscribe(dados => console.log(dados));
-      }
-    }
-}
- */
