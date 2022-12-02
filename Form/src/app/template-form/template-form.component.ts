18dcc4de4587bbc5582ee3a8717bd04cce4d1c51
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2022-12-02 07:38:26 -0300

    limpo

--- Form/src/app/template-form/template-form.component.ts
+++ Form/src/app/template-form/template-form.component.ts
@@ -17,7 +17,6 @@ export class TemplateFormComponent implements OnInit {
     console.log(formulario);
     console.log(this.usuario);
 
-
     this.http.post('https://httpbin.org/post', JSON.stringify(formulario.value))
     .pipe(map(dados => dados))
     .subscribe(dados => { console.log(dados);
